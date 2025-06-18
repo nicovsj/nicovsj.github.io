94baa881225cf54657c9782450b3d38f28e21a2c
Nicolás Van Sint Jan <nicovsj@pm.me>
2025-06-18 15:25:45 +0200

    🔧 Update site URL in Astro configuration from GitHub Pages to personal domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://nicovsj.github.io',
+    site: 'https://nicovsj.me',
 
     output: 'static',
 });
