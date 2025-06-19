e13abe0472a223ba67e563fc61dac792e56889f6
Nicolás Van Sint Jan <nicovsj@pm.me>
2025-06-19 02:34:34 +0200

    🔧 Update ESLint configuration

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,7 +1,7 @@
 module.exports = {
   extends: [
     'eslint:recommended',
-    '@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended',
     'plugin:astro/recommended',
   ],
   parser: '@typescript-eslint/parser',
